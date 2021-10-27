5c27e003026f685e59a5a7e182deb89be76f207b
GinoGreen <luigiverde97@gmail.com>
2021-10-27 18:41:34 +0200

    Esercitazione completata full javascript + bonus animazioni

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ const text = [
 ];
 
 const sliderLeft = document.getElementById('slider-left');
+const sliderRight = document.getElementById('slider-right');
 let contatore = 0;
 // vettore immagini preview active 
 const itemsPreview = document.getElementsByClassName('item-mini');
@@ -32,12 +33,16 @@ const itemsPreview = document.getElementsByClassName('item-mini');
 for (let i = 0; i < images.length; i++) {
 
    const item = document.createElement('div');
+   const itemMini = document.createElement('div');
    sliderLeft.append(item);
+   sliderRight.append(itemMini);
    item.className = 'item';
+   itemMini.className = 'item-mini';
 
    // il primo elemento avra la classe active di deafult
    if (i === contatore) {
       item.classList.add('active');
+      itemMini.classList.add('active');
       itemsPreview[contatore].classList.add('active');
    }
 
@@ -48,9 +53,12 @@ for (let i = 0; i < images.length; i++) {
          <p>${text[i]}</p>
       </div>
    `;
+   itemMini.innerHTML = ` <img src="${images[i]}" alt=""> `;
 
    console.log(item);
 }
+
+
 
 //********** */ carosello by clicking ********
 // costanti e vettore item
@@ -58,8 +66,13 @@ const items = document.getElementsByClassName('item');
 
 console.log('items',items);
 
-const upArrow = document.getElementById('up_arrow');
-const downArrow = document.getElementById('down_arrow');
+const upArrow = document.createElement('div');
+const downArrow = document.createElement('div');
+upArrow.className = 'up_arrow';
+downArrow.className = 'down_arrow';
+upArrow.innerHTML = '&uarr;';
+downArrow.innerHTML = '&darr;';
+sliderRight.append(upArrow, downArrow);
 
 // evento click freccia in su
 upArrow.addEventListener('click', function() {
